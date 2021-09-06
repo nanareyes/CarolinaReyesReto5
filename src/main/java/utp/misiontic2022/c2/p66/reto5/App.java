8579e2993d8f1d8edf530199af0ff780b52454bb
nanareyes <dianac.reyesmantilla@gmail.com>
2021-09-06 10:33:34 -0500

    Se crea carpeta con iformación básica para creación de página en Html

--- src/main/java/utp/misiontic2022/c2/p66/reto5/App.java
+++ src/main/java/utp/misiontic2022/c2/p66/reto5/App.java
@@ -9,16 +9,6 @@ import utp.misiontic2022.c2.p66.reto5.vista.VentanaGenerarReporte;
  */
 public class App {
     public static void main(String[] args) {
-//        System.out.println("Requerimiento 1");
-//        VistaRequerimientos.requerimiento1();
-//
-//        System.out.println("\nRequerimiento 2");
-//        VistaRequerimientos.requerimiento2();
-//
-//        System.out.println("\nRequerimiento 3");
-//        VistaRequerimientos.requerimiento3();
-//        VentanaMenu ventanaMenu = new VentanaMenu();
-//        ventanaMenu.setVisible(true);
         VentanaGenerarReporte ventanaReporte = new VentanaGenerarReporte();
         ventanaReporte.setVisible(true);
     }
